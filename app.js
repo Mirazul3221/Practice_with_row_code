76a6489a979bc7991e17efec6a53e5a7d67014cb
Mirazul Islam <mirazul3221@gmail.com>
2023-12-25 10:10:50 -0800

    U

--- app.js
+++ app.js
@@ -55,83 +55,83 @@
 // isStudent()
 
 // =====================================================================
-let student = true;
+// let student = true;
 
-let pp = 80;
+// let pp = 80;
 
-let marks = 70;
+// let marks = 70;
 
-function initialStep(callBack) {
-  setTimeout(() => {
-    if (student) {
-      callBack();
-    } else {
-      console.log('Admit first to be a student');
-    }
-  }, 1000);
-}
+// function initialStep(callBack) {
+//   setTimeout(() => {
+//     if (student) {
+//       callBack();
+//     } else {
+//       console.log('Admit first to be a student');
+//     }
+//   }, 1000);
+// }
 
-function progress(callBack) {
-  console.log('Attend to the class regularly and Prepar for the exam');
+// function progress(callBack) {
+//   console.log('Attend to the class regularly and Prepar for the exam');
 
-  setTimeout(() => {
-    if (pp >= 80) {
-      callBack();
-    } else {
-      console.log('Your addendance is too low to addend the exam');
-    }
-  }, 3000);
-}
+//   setTimeout(() => {
+//     if (pp >= 80) {
+//       callBack();
+//     } else {
+//       console.log('Your addendance is too low to addend the exam');
+//     }
+//   }, 3000);
+// }
 
-function exam(callBack) {
-  console.log('Your exam is knocking at the door. So you shuld study hard');
-  setTimeout(() => {
-    if (marks >= 80) {
-      console.log('waiting for the certificate....');
-      callBack();
-    } else {
-      console.log('you deserve no certificate');
-    }
-  }, 2000);
-}
+// function exam(callBack) {
+//   console.log('Your exam is knocking at the door. So you shuld study hard');
+//   setTimeout(() => {
+//     if (marks >= 80) {
+//       console.log('waiting for the certificate....');
+//       callBack();
+//     } else {
+//       console.log('you deserve no certificate');
+//     }
+//   }, 2000);
+// }
 
-function getCertificate() {
-  setTimeout(() => {
-    console.log('Congrate! you got the certificate');
-  }, 3000);
-}
+// function getCertificate() {
+//   setTimeout(() => {
+//     console.log('Congrate! you got the certificate');
+//   }, 3000);
+// }
 
-initialStep(() => {
-  progress(() => {
-    exam(getCertificate);
-  });
-});
+// initialStep(() => {
+//   progress(() => {
+//     exam(getCertificate);
+//   });
+// });
 // ======================================
 
-function handleSubmit() {
-  let input = document.getElementById('demo').value;
+// function handleSubmit() {
+//   let input = document.getElementById('demo').value;
 
-  let index = [];
-  for (let i = 0; i < input; i++) {
-    index.push(i + 1);
-  }
+//   let index = [];
+//   for (let i = 0; i < input; i++) {
+//     index.push(i + 1);
+//   }
 
-  let total = 0;
-  index.forEach(i => (total += i));
-  alert(total);
+//   let total = 0;
+//   index.forEach(i => (total += i));
+//   alert(total);
 
-  let prime = [];
-  prime.push(1);
+//   let prime = [];
+//   prime.push(1);
 
-  startAgain: for (let outer = 2; outer <= input; outer++) {
-    //7
-    for (let inner = 2; inner < outer; inner++) {
-      if (outer % inner == 0) {
-        continue startAgain;
-      }
-    }
-    prime.push(outer);
-  }
+//   startAgain: for (let outer = 2; outer <= input; outer++) {
+//     //7
+//     for (let inner = 2; inner < outer; inner++) {
+//       if (outer % inner == 0) {
+//         continue startAgain;
+//       }
+//     }
+//     prime.push(outer);
+//   }
 
   //   const total = index => {
   //     let num = 0;
@@ -140,10 +140,10 @@ function handleSubmit() {
   //     }
   //   };
 
-  console.log(prime);
+//   console.log(prime);
 
-  console.log(prime.length);
-}
+//   console.log(prime.length);
+// }
 
 // console.log("This message is printed after the first message is executed")
 
@@ -162,33 +162,33 @@ function handleSubmit() {
 // =============================================================
 // =============================================================
 
-let obj = {
-  mane : "Rahim",
-  email:"rohim@gmail.com",
-  age:17,
-  weight: "50 Kg",
-  main:this
-}
+// let obj = {
+//   mane : "Rahim",
+//   email:"rohim@gmail.com",
+//   age:17,
+//   weight: "50 Kg",
+//   main:this
+// }
 
 
-console.log(obj.email)
+// console.log(obj.email)
 
 // console.log(new obj)
 
-const StudentFrame = function(name){
- this.name = name;
-}
+// const StudentFrame = function(name){
+//  this.name = name;
+// }
 
-const karim =  new StudentFrame('karim')
-const rahim =  new StudentFrame('rahim')
-const sofiq =  new StudentFrame('sofiq')
-console.log(karim,rahim,sofiq)
-StudentFrame.prototype.aboutI = function (){
-  console.log(`My name is ${this.name}`)
- }
+// const karim =  new StudentFrame('karim')
+// const rahim =  new StudentFrame('rahim')
+// const sofiq =  new StudentFrame('sofiq')
+// console.log(karim,rahim,sofiq)
+// StudentFrame.prototype.aboutI = function (){
+//   console.log(`My name is ${this.name}`)
+//  }
 
- console.log(StudentFrame.prototype.__proto__)
- console.log(karim.__proto__.__proto__.__proto__)
+//  console.log(StudentFrame.prototype.__proto__)
+//  console.log(karim.__proto__.__proto__.__proto__)
 
 // console.log(sofiq.aboutI())
 
@@ -203,5 +203,37 @@ StudentFrame.prototype.aboutI = function (){
 
 // console.log(arr.filter(ele=> ele))
 
+class User{
+  constructor(name,email,desc,mobile){
+    this.name = name;
+    this.email = email;
+    this.desc = desc;
+    this.mobile = mobile
+  }
+
+  details(){
+    console.log(`${this.name} has an ${this.email} and ${this.desc} and also has a ${this.mobile}`)
+  }
+}
+
+const rahim = new User('Rohim',"rohim@gmail.com","He is a doctor",23423412)
+rahim.details()
+// const kohim = new User('Korim',"kohim@gmail.com","He is a farmer",23423464652)
+
+
+class Modify extends User{
+  constructor(name,email,desc,mobile,facebookId){
+    super(name,email,desc,mobile)
+    this.facebookId = facebookId
+  }
+  
+  details(){
+    console.log(`${this.name} has an ${this.email} and ${this.desc} and also has a ${this.mobile} and ${this.facebookId}`)
+  }
+}
+
+const korim = new Modify('Korim',"kohim@gmail.com","He is a farmer",23423464652,'http://fb.com/hfdssjh/afhsdjhk')
+
+korim.details()
 
 
